4f2396bd4b4dc35dbf726664f60bbe7f7bb8d819
Ed Wantuil <ed@wantuil.com>
2024-05-20 09:07:14 -0300

    Refactor integration tests and update GitHub workflow
    Detailed permissions were added to test-pr.yml. Customer and Product integration tests were removed, and a new integration test for Order was introduced. Additionally, utilities for the initial setup of DynamoDB were added to the testing environment.

--- src/integrationTest/java/br/com/fiap/techchallenge/KarateTests.java
+++ src/integrationTest/java/br/com/fiap/techchallenge/KarateTests.java
@@ -1,30 +1,65 @@
 package br.com.fiap.techchallenge;
 
+import java.io.IOException;
+
+import br.com.fiap.techchallenge.util.DynamoInit;
 import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Tag("integrationTest")
+@Testcontainers
 @SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application-testIntegration.yml")
 @ActiveProfiles("testIntegration")
 public class KarateTests {
 
+    public static final String SQS_QUEUE_PAGAMENTO_NAME = "tech_challenge_pagamento";
+    public static final String SQS_QUEUE_PEDIDO_NAME = "tech_challenge_pedido";
+
     @LocalServerPort
     private int localServerPort;
 
-    @Karate.Test
-    Karate customerTest() {
-        return Karate.run("file:src/integrationTest/java/br/com/fiap/techchallenge/feature/Customer.feature")
-                .systemProperty("karate.port", String.valueOf(localServerPort));
+    @Container
+    static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.2"))
+            .withServices(LocalStackContainer.Service.SQS,
+                    LocalStackContainer.Service.DYNAMODB);
+
+    @BeforeAll
+    public static void beforeAll() throws IOException, InterruptedException {
+        localStackContainer.execInContainer("awslocal", "sqs", "create-queue", "--queue-name", SQS_QUEUE_PAGAMENTO_NAME);
+        localStackContainer.execInContainer("awslocal", "sqs", "create-queue", "--queue-name", SQS_QUEUE_PEDIDO_NAME);
+
+        final String endpoint = localStackContainer.getEndpointOverride(LocalStackContainer.Service.DYNAMODB).toString();
+        System.setProperty("aws.dynamodb.endpoint", endpoint);
+        System.setProperty("aws.sqs.endpoint", endpoint);
+
+        final String region = localStackContainer.getRegion();
+        System.setProperty("aws.region", region);
+
+        final String accessKey = localStackContainer.getAccessKey();
+        System.setProperty("aws.accessKey", accessKey);
+
+        final String secretKey = localStackContainer.getSecretKey();
+        System.setProperty("aws.secretKey", secretKey);
+
+        final String sessionToken = localStackContainer.getSecretKey();
+        System.setProperty("aws.sessionToken", sessionToken);
+
+        DynamoInit.init(endpoint, region, accessKey, secretKey);
     }
 
     @Karate.Test
-    Karate productTest() {
-        return Karate.run("file:src/integrationTest/java/br/com/fiap/techchallenge/feature/Product.feature")
+    Karate orderTest() {
+        return Karate.run("file:src/integrationTest/java/br/com/fiap/techchallenge/feature/Order.feature")
                 .systemProperty("karate.port", String.valueOf(localServerPort));
     }
 }
